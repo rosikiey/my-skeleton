13209279010552516e5fc10af6d5d88a06ed30b5
rosikiey <rosi.app.project@gmail.com>
2024-01-10 21:29:39 +0700

    fix footer

--- src/routes/footer.svelte
+++ src/routes/footer.svelte
@@ -13,12 +13,12 @@
 			</div>
 			<div class="basis-full text-center pt-4">
 				<h3 class="text-lg md:text-lg font-bold leading-7 text-gray-400 font_gelaso">
-					Aplikasi Pro di rancang untuk memudahkan proses bisnis anda dimanapun dan kapanpun.
+				  Team Brackets di bentuk untuk memudahkan proses bisnis anda dimanapun dan kapanpun.
 				</h3>
 			</div>
 			<div class="basis-full text-center">
 				<h3 class="text-lg md:text-lg font-bold leading-7 text-gray-400 font_gelaso">
-					Support Android dan IOS !!!
+					Software Support Android dan IOS !!!
 				</h3>
 			</div>
 		</div>
